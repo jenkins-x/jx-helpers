aa620e0871166d451f666235614e5fda6baf1f41
James Strachan <james.strachan@gmail.com>
2020-09-17 06:07:43 +0100

    fix: add helper method

---
+++ pkg/scmhelpers/labels.go
@@ -0,0 +1,13 @@
+package scmhelpers
+
+import "github.com/jenkins-x/go-scm/scm"
+
+// ContainsLabel returns true if the given labels contains the given label name
+func ContainsLabel(labels []*scm.Label, label string) bool {
+	for _, l := range labels {
+		if l != nil && l.Name == label {
+			return true
+		}
+	}
+	return false
+}
