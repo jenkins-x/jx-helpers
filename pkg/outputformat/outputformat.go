26ecb99da5af99d4285826970d32244ef8cada4b
James Strachan <james.strachan@gmail.com>
2020-09-18 10:08:37 +0100

    fix: add a helper command for outputting yaml/json

---
+++ pkg/outputformat/outputformat.go
@@ -0,0 +1,31 @@
+package outputformat
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+
+	"sigs.k8s.io/yaml"
+)
+
+// Marshal marshals the value to the given output stream with a format string using yaml or json
+func Marshal(value interface{}, out io.Writer, format string) error {
+	switch format {
+	case "json":
+		data, err := json.Marshal(value)
+		if err != nil {
+			return err
+		}
+		_, e := fmt.Fprintf(out, string(data))
+		return e
+	case "yaml":
+		data, err := yaml.Marshal(value)
+		if err != nil {
+			return err
+		}
+		_, e := fmt.Fprintf(out, string(data))
+		return e
+	default:
+		return fmt.Errorf("Unsupported output format: %s", format)
+	}
+}
