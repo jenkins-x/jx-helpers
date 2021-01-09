e975f875cecfa5194d7589b5b4d6d482daaa241b
ryan.neal <ryan.neal@bt.com>
2021-01-09 14:01:00 +0000

    added SCM Not Found check utility based on response

---
+++ pkg/scmhelpers/repo_factory_test.go
@@ -0,0 +1,32 @@
+package scmhelpers_test
+
+import (
+	"github.com/jenkins-x/go-scm/scm"
+	"github.com/jenkins-x/jx-helpers/v3/pkg/scmhelpers"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestResponseNotFoundShouldBeFalseWhenNoResponseProvided(t *testing.T) {
+	f := scmhelpers.IsScmResponseNotFound(nil)
+
+	assert.Falsef(t, f, "should be false when response is nil")
+}
+
+func TestResponseNotFoundShouldBeFalseWhenResponseStatusIsOk(t *testing.T) {
+	res := new(scm.Response)
+	res.Status = 200
+
+	f := scmhelpers.IsScmResponseNotFound(res)
+
+	assert.Falsef(t, f, "should be false when response has 200 status")
+}
+
+func TestResponseNotFoundShouldBeFalseWhenResponseStatusIsNotFound(t *testing.T) {
+	res := new(scm.Response)
+	res.Status = 404
+
+	f := scmhelpers.IsScmResponseNotFound(res)
+
+	assert.Truef(t, f, "should be true when response has 404 status")
+}
