bdba343418d591668efaa8d5ad99bd71b21511fe
Mårten Svantesson <Marten.Svantesson@ticket.se>
2023-08-09 12:04:28 +0200

    feat: support adding only kind or selector part of filter flags

--- pkg/kyamls/filter.go
+++ pkg/kyamls/filter.go
@@ -95,12 +95,22 @@ func trimQuotes(text string) string {
 
 // AddFlags add CLI flags for specifying a filter
 func (f *Filter) AddFlags(cmd *cobra.Command) {
+	f.AddKindFlags(cmd)
+	f.AddSelectorFlags(cmd)
+}
+
+// AddKindFlags add CLI flags for specifying the kind part of a filter
+func (f *Filter) AddKindFlags(cmd *cobra.Command) {
 	cmd.Flags().StringArrayVarP(&f.Kinds, "kind", "k", nil,
 		"adds Kubernetes resource kinds to filter on. For kind expressions see:"+
 			" https://github.com/jenkins-x/jx-helpers/tree/master/docs/kind_filters.md")
 	cmd.Flags().StringArrayVarP(&f.KindsIgnore, "kind-ignore", "", nil,
 		"adds Kubernetes resource kinds to exclude. For kind expressions see:"+
 			" https://github.com/jenkins-x/jx-helpers/tree/master/docs/kind_filters.md")
+}
+
+// AddSelectorFlags add CLI flags for specifying the selector part of a filter
+func (f *Filter) AddSelectorFlags(cmd *cobra.Command) {
 	cmd.Flags().StringToStringVarP(&f.Selector, "selector", "", nil,
 		"adds Kubernetes label selector to filter on, e.g. -s app=pusher-wave,heritage=Helm")
 	cmd.Flags().StringVar(&f.SelectTarget, "selector-target", "",
