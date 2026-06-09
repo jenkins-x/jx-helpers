29428c8b1043bae41d52829d6890597cfdd2b8ea
mentlak <mentlak@outlook.com>
2026-06-09 15:13:38 +0100

    chore: upgrade golint v2.12.0

--- hack/linter.sh
+++ hack/linter.sh
@@ -11,36 +11,26 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if ! [ -x "$(command -v golangci-lint)" ]; then
 	echo "Installing GolangCI-Lint"
-	${DIR}/install_golint.sh -b $GOPATH/bin v1.30.0
+	${DIR}/install_golint.sh -b $GOPATH/bin v2.12.0
 fi
 
 export GO111MODULE=on
 golangci-lint run \
 	--no-config \
-  --disable-all \
-  -E golint \
-  -E varcheck \
+  --default=none \
+  -E revive \
+  -E unused \
   -E errcheck \
 	-E misspell \
 	-E unconvert \
-  -E deadcode \
-  -E unconvert \
   -E gosec \
-  -E gofmt \
-  -E goimports \
-  -E structcheck \
-  -E interfacer \
   -E govet \
   -E unparam \
-  -E megacheck \
+  -E staticcheck \
   -E goconst \
   -E ineffassign \
-  -E unparam \
   -E gocritic \
-  -E maligned \
-  -E typecheck \
-  --skip-dirs vendor \
-  --deadline 15m0s \
+  --timeout 15m0s \
   --verbose \
   --build-tags build
 
